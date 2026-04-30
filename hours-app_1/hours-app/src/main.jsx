d5182f48b0ec8fc2ecb8d828cf9976e3acd44c67
Sharwaab-crypto <Sharwaab@icloud.com>
2026-04-30 17:42:10 +0800

    Update main.jsx

--- hours-app_1/hours-app/src/main.jsx
+++ hours-app_1/hours-app/src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Service Worker register
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch((err) => {
+      console.error("Service Worker register failed:", err);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
